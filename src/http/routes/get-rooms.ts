fcf05c27308569ce6ac148fc0eb0572c33b09a12
marceloabf <celo.filho1@hotmail.com>
2025-08-11 14:35:14 -0300

    [IA-LiveCompanion fix] Adjusting get rooms endpoint to use userId

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,22 +1,39 @@
 import { count, eq } from "drizzle-orm";
 import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
+import z from "zod/v4";
 import { db } from "../../db/connection.ts";
 import { schema } from "../../db/schema/index.ts";
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
-  app.get("/rooms", async () => {
-    const results = await db
-      .select({
-        id: schema.rooms.id,
-        name: schema.rooms.name,
-        createdAt: schema.rooms.createdAt,
-        questionsCount: count(schema.questions.id),
-      })
-      .from(schema.rooms)
-      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
-      .groupBy(schema.rooms.id)
-      .orderBy(schema.rooms.createdAt);
+  app.get(
+    "/rooms/:userId",
+    {
+      schema: {
+        params: z.object({
+          userId: z.uuid(),
+        }),
+      },
+    },
+    async (request) => {
+      const { userId } = request.params;
 
-    return results;
-  });
+      const results = await db
+        .select({
+          id: schema.rooms.id,
+          name: schema.rooms.name,
+          createdAt: schema.rooms.createdAt,
+          questionsCount: count(schema.questions.id),
+        })
+        .from(schema.rooms)
+        .leftJoin(
+          schema.questions,
+          eq(schema.questions.roomId, schema.rooms.id)
+        )
+        .where(eq(schema.rooms.userId, userId))
+        .groupBy(schema.rooms.id)
+        .orderBy(schema.rooms.createdAt);
+
+      return results;
+    }
+  );
 };
